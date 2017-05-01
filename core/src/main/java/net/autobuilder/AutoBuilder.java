87551df04cc2e2ed482a50f01ed47cd83b4f0e8b
h908714124 <kraftdurchblumen@gmx.de>
2017-05-01 20:48:54 +0200

    document the annotation

--- core/src/main/java/net/autobuilder/AutoBuilder.java
+++ core/src/main/java/net/autobuilder/AutoBuilder.java
@@ -5,6 +5,18 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Marker annotation for
+ * <a href="https://github.com/h908714124/auto-builder">AutoBuilder</a>.
+ * <ul>
+ * <p>
+ * <li>This won't do anything,
+ * unless the class also has an @AutoValue annotation.</li>
+ * <p>
+ * <li>The annotated class should be a "regular" auto-value class,
+ * not the "builder" variety.</li>
+ * </ul>
+ */
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.SOURCE)
 public @interface AutoBuilder {
