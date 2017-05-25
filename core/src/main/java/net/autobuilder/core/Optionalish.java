5ce48321b509084b9e43751df22bd94e191bef3a
h908714124 <kraftdurchblumen@gmx.de>
2017-05-25 22:58:33 +0200

    use javax.lang.model in Optionalish.create

--- core/src/main/java/net/autobuilder/core/Optionalish.java
+++ core/src/main/java/net/autobuilder/core/Optionalish.java
@@ -5,31 +5,33 @@ import com.squareup.javapoet.CodeBlock;
 import com.squareup.javapoet.FieldSpec;
 import com.squareup.javapoet.MethodSpec;
 import com.squareup.javapoet.ParameterSpec;
-import com.squareup.javapoet.ParameterizedTypeName;
 import com.squareup.javapoet.TypeName;
 
+import javax.lang.model.element.TypeElement;
+import javax.lang.model.type.DeclaredType;
+import javax.lang.model.type.TypeMirror;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
-import java.util.OptionalDouble;
-import java.util.OptionalInt;
-import java.util.OptionalLong;
 
 import static javax.lang.model.element.Modifier.FINAL;
-import static net.autobuilder.core.AutoBuilderProcessor.rawType;
+import static net.autobuilder.core.Util.AS_DECLARED;
+import static net.autobuilder.core.Util.AS_TYPE_ELEMENT;
+import static net.autobuilder.core.Util.equalsType;
 
 final class Optionalish extends ParaParameter {
 
+  private static final String JAVA_UTIL_OPTIONAL = "java.util.Optional";
   private static final ClassName OPTIONAL_CLASS =
       ClassName.get(Optional.class);
 
-  private static final Map<ClassName, TypeName> OPTIONAL_PRIMITIVES;
+  private static final Map<String, TypeName> OPTIONAL_PRIMITIVES;
 
   static {
     OPTIONAL_PRIMITIVES = new HashMap<>(3);
-    OPTIONAL_PRIMITIVES.put(ClassName.get(OptionalInt.class), TypeName.INT);
-    OPTIONAL_PRIMITIVES.put(ClassName.get(OptionalDouble.class), TypeName.DOUBLE);
-    OPTIONAL_PRIMITIVES.put(ClassName.get(OptionalLong.class), TypeName.LONG);
+    OPTIONAL_PRIMITIVES.put("java.util.OptionalInt", TypeName.INT);
+    OPTIONAL_PRIMITIVES.put("java.util.OptionalDouble", TypeName.DOUBLE);
+    OPTIONAL_PRIMITIVES.put("java.util.OptionalLong", TypeName.LONG);
   }
 
   private static final String OF_NULLABLE = "ofNullable";
@@ -53,28 +55,32 @@ final class Optionalish extends ParaParameter {
   }
 
   static Optional<ParaParameter> create(Parameter parameter) {
-    if (parameter.type instanceof ClassName) {
-      ClassName className = rawType(parameter.type);
-      TypeName primitive = OPTIONAL_PRIMITIVES.get(className);
+    TypeMirror type = parameter.variableElement.asType();
+    DeclaredType declaredType = type.accept(AS_DECLARED, null);
+    if (declaredType == null) {
+      return Optional.empty();
+    }
+    TypeElement typeElement = declaredType.asElement().accept(AS_TYPE_ELEMENT, null);
+    if (typeElement == null) {
+      return Optional.empty();
+    }
+    if (declaredType.getTypeArguments().size() > 1) {
+      return Optional.empty();
+    }
+    if (declaredType.getTypeArguments().isEmpty()) {
+      TypeName primitive = OPTIONAL_PRIMITIVES.get(typeElement.getQualifiedName().toString());
       return primitive != null ?
-          Optional.of(new Optionalish(parameter, className, primitive, "of")) :
+          Optional.of(new Optionalish(parameter, ClassName.get(typeElement), primitive, "of")) :
           Optional.empty();
     }
-    if (!(parameter.type instanceof ParameterizedTypeName)) {
+    if (!equalsType(type, JAVA_UTIL_OPTIONAL)) {
       return Optional.empty();
     }
-    ParameterizedTypeName type = (ParameterizedTypeName) parameter.type;
-    if (!type.rawType.equals(OPTIONAL_CLASS)) {
+    if (equalsType(declaredType.getTypeArguments().get(0), JAVA_UTIL_OPTIONAL)) {
       return Optional.empty();
     }
-    if (type.typeArguments.size() != 1) {
-      return Optional.empty();
-    }
-    TypeName wrapped = type.typeArguments.get(0);
-    if (rawType(wrapped).equals(OPTIONAL_CLASS)) {
-      return Optional.empty();
-    }
-    return Optional.of(new Optionalish(parameter, type.rawType, wrapped,
+    return Optional.of(new Optionalish(parameter, OPTIONAL_CLASS,
+        TypeName.get(declaredType.getTypeArguments().get(0)),
         OF_NULLABLE));
   }
 
--- core/src/main/java/net/autobuilder/core/Util.java
+++ core/src/main/java/net/autobuilder/core/Util.java
@@ -11,6 +11,7 @@ import javax.lang.model.element.VariableElement;
 import javax.lang.model.type.DeclaredType;
 import javax.lang.model.type.TypeMirror;
 import javax.lang.model.type.TypeVariable;
+import javax.lang.model.util.SimpleElementVisitor8;
 import javax.lang.model.util.SimpleTypeVisitor8;
 import java.util.ArrayList;
 import java.util.HashSet;
@@ -47,13 +48,33 @@ final class Util {
         }
       };
 
-  private static final SimpleTypeVisitor8<DeclaredType, Void> AS_DECLARED =
+  static final SimpleTypeVisitor8<DeclaredType, Void> AS_DECLARED =
       new SimpleTypeVisitor8<DeclaredType, Void>() {
         @Override
         public DeclaredType visitDeclared(DeclaredType declaredType, Void _null) {
           return declaredType;
         }
       };
+
+  static final SimpleElementVisitor8<TypeElement, Void> AS_TYPE_ELEMENT =
+      new SimpleElementVisitor8<TypeElement, Void>() {
+        @Override
+        public TypeElement visitType(TypeElement typeElement, Void _null) {
+          return typeElement;
+        }
+      };
+
+  static boolean equalsType(TypeMirror typeMirror, String qualified) {
+    DeclaredType declared = typeMirror.accept(AS_DECLARED, null);
+    if (declared == null) {
+      return false;
+    }
+    TypeElement typeElement = declared.asElement().accept(AS_TYPE_ELEMENT, null);
+    if (typeElement == null) {
+      return false;
+    }
+    return typeElement.getQualifiedName().toString().equals(qualified);
+  }
 
   private final ProcessingEnvironment processingEnv;
 
